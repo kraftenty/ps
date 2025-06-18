07e2aa2c2da95b5ec451db67ee6275b37caf2b39
고경태 <kraftenty@gmail.com>
2025-06-18 16:08:55 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 25.89 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -3,13 +3,13 @@ import java.util.*;
 class Solution {
 	
 	private Map<String, Integer> courseMap; // 코스, 중복개수
-	private Set<Integer> courseLengthSet; // 코스 길이 셋
+	private Map<Integer, Integer> courseMaxCountMap; // 코스 길이, 최대 주문횟수
 	
     public String[] solution(String[] orders, int[] course) {
     	courseMap = new HashMap<>();
-    	courseLengthSet = new HashSet<>();
+    	courseMaxCountMap = new HashMap<>();
     	for (int c : course) {
-    		courseLengthSet.add(c);
+    		courseMaxCountMap.put(c, 0);
     	}
     	
     	for (String order : orders) {
@@ -20,15 +20,9 @@ class Solution {
 
     	
     	List<String> result = new ArrayList<>();
-    	for (int len : courseLengthSet) {
+    	for (int len : course) {
     		// maxCount : len 길이를 가진 코스 중에서 최대 개수
-    		int maxCount = 0;
-    		for (Map.Entry<String, Integer> entry : courseMap.entrySet()) {
-    			if (entry.getKey().length() == len && entry.getValue() >= 2) {
-    				maxCount = Math.max(maxCount, entry.getValue());
-    			}
-    		}
-    		
+    		int maxCount = courseMaxCountMap.get(len);
     		if (maxCount >= 2) {
     			for (Map.Entry<String, Integer> entry : courseMap.entrySet()) {
     				if (entry.getKey().length() == len && entry.getValue() == maxCount) {
@@ -44,8 +38,11 @@ class Solution {
     
     // 조합 생성
     public void combinations(int idx, char[] arr, String s) {
-    	if (s.length() > 0 && courseLengthSet.contains(s.length())) {
+    	if (s.length() > 0 && courseMaxCountMap.containsKey(s.length())) {
+    		// 해당 조합의 주문횟수 업데이트
     		courseMap.put(s, courseMap.getOrDefault(s, 0) + 1);
+    		// 해당 조합의 최대 주문 횟수 저장
+    		courseMaxCountMap.put(s.length(), Math.max(courseMaxCountMap.get(s.length()), courseMap.get(s)));
     	}
     	
     	for (int i=idx; i<arr.length; i++) {
