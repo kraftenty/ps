a0b80143e1798eb2eddac43d9f1c26141fbda1b1
고경태 <kraftenty@gmail.com>
2025-10-28 14:42:29 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 64 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,15 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-class Node {
-	int y, x, d;
-	public Node (int y, int x, int d) { // d)0=가로, 1=대각, 2세로
-		this.y=y;
-		this.x=x;
-		this.d=d;
-	}
-}
-
 public class Main {
 	
 	static int N;
@@ -31,40 +22,40 @@ public class Main {
 			return;
 		}
 		
-		Node s = new Node(0, 1, 0);
-		Queue<Node> q = new ArrayDeque<>();
-		q.offer(s);
-		
-		int cnt = 0;
-		while (!q.isEmpty()) {
-			Node cur = q.poll();
-			if (cur.y == N-1 && cur.x == N-1) {
-				cnt++;
-			}
-			
-			if (cur.d==0 || cur.d==1) { // 가로
-				if (inArea(cur.y, cur.x+1, false)) {
-					q.add(new Node(cur.y, cur.x+1, 0));
+		int[][][] dp = new int[N][N][3]; // d)0=가로, 1=대각, 2세로
+		dp[0][1][0] = 1; // dp[y][x][d]의 경우의 수 = n개
+		for (int y=0; y<N; y++) {
+			for (int x=0; x<N; x++) {
+				if (board[y][x]) continue;
+				
+				// 가로
+				if (dp[y][x][0] > 0) {
+					if (inArea(y, x+1, false)) dp[y][x+1][0] += dp[y][x][0]; // 가로
+					if (inArea(y+1, x+1, true)) dp[y+1][x+1][1] += dp[y][x][0]; // 대각
 				}
-			}
-			if (cur.d==1 || cur.d==2) { // 세로
-				if (inArea(cur.y+1, cur.x, false)) {
-					q.add(new Node(cur.y+1, cur.x, 2));
+				
+				// 대각
+				if (dp[y][x][1] > 0) {
+					if (inArea(y, x+1, false)) dp[y][x+1][0] += dp[y][x][1]; // 가로
+					if (inArea(y+1, x+1, true)) dp[y+1][x+1][1] += dp[y][x][1]; // 대각
+					if (inArea(y+1, x, false)) dp[y+1][x][2] += dp[y][x][1]; // 세로
 				}
-			}
-			// 대각
-			if (inArea(cur.y+1, cur.x+1, true)) {
-				q.add(new Node(cur.y+1, cur.x+1, 1));
+				
+				// 세로
+				if (dp[y][x][2] > 0) {
+					if (inArea(y+1, x+1, true)) dp[y+1][x+1][1] += dp[y][x][2]; // 대각
+					if (inArea(y+1, x, false)) dp[y+1][x][2] += dp[y][x][2]; // 세로
+				}
 			}
 		}
 		
-		System.out.println(cnt);
+		System.out.println(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2]);
 	}
 	
 	static boolean inArea(int y, int x, boolean isWide) {
 		boolean b = y<N && x<N && !board[y][x];
 		if (isWide && b) {
-			b = b && (y-1>=0 && y-1<N) && (x-1>=0 && x-1<N) && !board[y-1][x] && !board[y][x-1];
+			b = b && (y-1<N) && (x-1<N) && !board[y-1][x] && !board[y][x-1];
 		}
 		return b;
 	}
