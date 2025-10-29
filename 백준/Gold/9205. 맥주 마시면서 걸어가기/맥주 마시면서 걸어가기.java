16beb239437a1a118c0323d85f016beba91eb238
고경태 <kraftenty@gmail.com>
2025-10-29 21:27:02 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 72 ms, Memory: 12564 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -2,8 +2,9 @@ import java.util.*;
 import java.io.*;
 
 class Node {
-	int y,x,idx,v;
-	public Node(int y, int x, int idx, int v) {
+	int y,x,idx;
+	boolean v;
+	public Node(int y, int x, int idx, boolean v) {
 		this.y=y;
 		this.x=x;
 		this.idx=idx;
@@ -17,7 +18,7 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for (int tc=1; tc<=T; tc++) {
+		a: for (int tc=1; tc<=T; tc++) {
 			int N = Integer.parseInt(br.readLine()); // 편의점 개수
 			
 			Node[] nodearr = new Node[N+2];
@@ -26,37 +27,36 @@ public class Main {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				int x = Integer.parseInt(st.nextToken());
 				int y = Integer.parseInt(st.nextToken());
-				nodearr[i] = new Node(y, x, i, 0);
+				nodearr[i] = new Node(y, x, i, false);
 			}
 			
 			Queue<Node> q = new ArrayDeque<>();
 			q.offer(nodearr[0]);
-			nodearr[0].v=1000;
+			nodearr[0].v=true;
 			while (!q.isEmpty()) {
 				Node cur = q.poll();
 				for (int i=1; i<N+2; i++) {
 					Node next = nodearr[i];
-					if (next.v == 0 && walk(cur.y, cur.x, next.y, next.x, cur.v) != -1) {
-						next.v = 1000;
+					if (!next.v && able(cur.y, cur.x, next.y, next.x)) {
+						if (next.idx==N+1) {
+							sb.append("happy").append("\n");
+							continue a;
+						}
+						next.v = true;
 						q.offer(next);
 					}
 				}
 			}
 			
-			if (nodearr[N+1].v == 0) {
-				sb.append("sad").append("\n");
-			} else {
-				sb.append("happy").append("\n");
-			}
+			sb.append("sad").append("\n");
 		}
 		
 		System.out.println(sb.toString());
 	}
 	
-	static int walk(int sy, int sx, int ey, int ex, int beer) {
+	static boolean able(int sy, int sx, int ey, int ex) {
 		int mhtdist = Math.abs(sy-ey) + Math.abs(sx-ex);
-		if (mhtdist > beer) return -1;
-		return beer - mhtdist;
+		return (mhtdist <= 1000) ? true : false;
 	}
 
 }
