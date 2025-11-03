413ceb9cc8c363f838d0f471654c7c4357cdfb4d
고경태 <kraftenty@gmail.com>
2025-11-03 22:28:23 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,17 @@
+select
+    r.rest_id,
+    i.rest_name,
+    i.food_type,
+    i.favorites,
+    i.address,
+    round(avg(r.review_score), 2) as score
+from
+    rest_review r join rest_info i on r.rest_id = i.rest_id
+where
+    i.address like '서울%'
+group by
+    r.rest_id
+order by
+    round(avg(r.review_score), 2) desc,
+    i.favorites desc
+    
