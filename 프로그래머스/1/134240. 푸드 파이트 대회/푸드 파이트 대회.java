0580488c85e6938d4bac0716cedf068fb9421bb8
고경태 <kraftenty@gmail.com>
2025-11-20 12:21:42 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.85 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=1; i<food.length; i++) {
+            for (int j=0; j<food[i]/2; j++) {
+                sb.append(i);
+            }
+        }
+        
+        StringBuilder rv = new StringBuilder(sb).reverse();
+        sb.append(0).append(rv);
+        return sb.toString();
+    }
+}
